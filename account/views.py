f713a641c5cdbabbc36193f3079728a18abfd08e
hus <hus@google.com>
2021-01-10 22:44:06 +0330

    add .

--- account/views.py
+++ account/views.py
@@ -4,22 +4,26 @@ from django.contrib.auth import authenticate, login
 from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
 
+
 def register(request):
     if request.method == 'POST':
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
-            #Create a new user object but avoid saving it yet
+            # Create a new user object but avoid saving it yet
             new_user = user_form.save(commit=False)
-            #set the chosen password
+            # Set the chosen password
             new_user.set_password(
                 user_form.cleaned_data['password'])
             # Save the User object
             new_user.save()
-            return  render(request, 'account/register_done.html',
-                           {'new_user': new_user})
-        else:
-            user_form = UserRegistrationForm()
-        return render(request, 'account/register.html',{'user_form' : user_form})
+            return render(request,
+                          'account/register_done.html',
+                          {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+        return render(request,
+                      'account/register.html',
+                      {'user_form': user_form})
 
 
 @login_required
